4f53f1c76ee0c6553b368b9a780c01510cbd2e61
khaledmju <khaledjoouu@gmail.com>
2024-12-06 22:57:50 +0300

    translation

---
+++ lib/local/local_contorller.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:onboar/main.dart';
+class MyLocaleController extends GetxController{
+
+  }
