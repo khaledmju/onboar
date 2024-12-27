c99170d4377398fabc7a9d166a5cc746bb6f68e7
khaledmju <khaledjoouu@gmail.com>
2024-12-27 21:44:21 +0300

    c

--- lib/ProductDetailsPage.dart
+++ lib/ProductDetailsPage.dart
@@ -17,193 +17,202 @@ class ProductDetailsPage extends StatefulWidget {
 }
 
 class _ProductDetailsPage extends State<ProductDetailsPage> {
-  int count=0;
+  int count = 0;
+
   bool isArabic() {
     return Get.locale?.languageCode == 'ar';
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-          appBar: AppBar(
-              leading: IconButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                icon: const Icon(Icons.arrow_back),
-              ),
-              title: Text("${widget.productData['title']}",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      color: Color.fromARGB(255, 20, 54, 64),
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal)),
-              centerTitle: true,
-              backgroundColor: const Color.fromARGB(255, 66, 252, 169),
-              elevation: 4,
-              shadowColor: const Color.fromARGB(255, 48, 193, 152),
-              actions: [
-                IconButton(
-                    onPressed: () {
-                      showSearch(context: context, delegate: SearchCustom());
-                    },
-                    icon: const Icon(Icons.search)),
-              ]),
-          body: Container(
-              color: Colors.white,
-              width: double.infinity,
-              child: ListView(
-                scrollDirection: Axis.vertical,
-                physics: const AlwaysScrollableScrollPhysics(),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+        title: Text("${widget.productData['title']}",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+                color: Color.fromARGB(255, 20, 54, 64),
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal)),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 66, 252, 169),
+        elevation: 4,
+        shadowColor: const Color.fromARGB(255, 48, 193, 152),
+      ),
+      body: Container(
+          color: Colors.white,
+          width: double.infinity,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            physics: const AlwaysScrollableScrollPhysics(),
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Image.asset(
-                        "${widget.productData['image']}",
-                        width: double.infinity,
-                        height: 200,
-                        fit: BoxFit.fitHeight,
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        // decoration: BoxDecoration(
-                        //   color : const Color.fromARGB(255, 66, 252, 169),
-                        //   border: Border.all(width: 2),
-                        //   borderRadius: BorderRadius.circular(10),
-                        // ),
-                        child: Text(
-                          "${widget.productData['title']}",
-                          style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 20, 54, 64)),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          "${widget.productData['subtitle']}",
-                          style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                              color: Color.fromARGB(255, 48, 193, 152)),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          textDirection: isArabic()
-                              ? TextDirection.ltr
-                              : TextDirection.rtl,
-                          children: [
-                            Text(
-                              "  ${widget.productData['quantity']}  ",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.grey.shade800,
-                              ),
-                            ),
-                            const Text(
-                              ":",
-                              style: TextStyle(
-                                  fontSize: 23, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            Text(
-                              "Quantity".tr,
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          textDirection: isArabic()
-                              ? TextDirection.ltr
-                              : TextDirection.rtl,
-                          children: [
-                            Text(
-                              "  ${widget.productData['price']}  ",
-                              style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.red),
-                            ),
-                            const Text(
-                              ":",
-                              style: TextStyle(
-                                  fontSize: 23, fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(
-                              width: 14,
-                            ),
-                            Text(
-                              "Price".tr,
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Image.asset(
+                    "${widget.productData['image']}",
+                    width: double.infinity,
+                    height: 200,
+                    fit: BoxFit.fitHeight,
                   ),
                   const SizedBox(height: 10),
-                  Center(child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton( onPressed: count > 0
-                          ? () {
-                        // Decrement logic
-                        setState(() {
-                          count--;
-                        });
-                      }
-                          : null,icon:Icon(Icons.remove_circle_outline,color: Colors.red,size: 30,)),
-                      Text("$count",style: TextStyle(fontSize: 25,),),
-                      IconButton(onPressed:() {
-                        // Increment logic
-                        setState(() {
-                          count++;
-                        });
-                      } ,icon:Icon(Icons.add_circle_outline,color: const Color.fromARGB(255, 66, 252, 169),size: 30,)),
-                    ],
-                  ),),
-                  Center(
-                    child: ElevatedButton(
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    // decoration: BoxDecoration(
+                    //   color : const Color.fromARGB(255, 66, 252, 169),
+                    //   border: Border.all(width: 2),
+                    //   borderRadius: BorderRadius.circular(10),
+                    // ),
+                    child: Text(
+                      "${widget.productData['title']}",
+                      style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 20, 54, 64)),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      "${widget.productData['subtitle']}",
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromARGB(255, 48, 193, 152)),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      textDirection:
+                          isArabic() ? TextDirection.ltr : TextDirection.rtl,
+                      children: [
+                        Text(
+                          "  ${widget.productData['quantity']}  ",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade800,
+                          ),
+                        ),
+                        const Text(
+                          ":",
+                          style: TextStyle(
+                              fontSize: 23, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 14,
+                        ),
+                        Text(
+                          "Quantity".tr,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      textDirection:
+                          isArabic() ? TextDirection.ltr : TextDirection.rtl,
+                      children: [
+                        Text(
+                          "  ${widget.productData['price']}  ",
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.red),
+                        ),
+                        const Text(
+                          ":",
+                          style: TextStyle(
+                              fontSize: 23, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 14,
+                        ),
+                        Text(
+                          "Price".tr,
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
                         onPressed: count > 0
                             ? () {
-                          // Add to cart logic
-                        }
+                                setState(() {
+                                  count--;
+                                });
+                              }
                             : null,
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 20, 54, 64),
-                            padding: const EdgeInsets.symmetric(horizontal: 40)),
-                        child: Text("Add to Cart".tr,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
-                              color: Color.fromARGB(255, 66, 252, 169),
-                            ))),
-                  )
-                ],
-              )),
-        );
+                        icon: Icon(
+                          Icons.remove_circle_outline,
+                          color: Colors.red,
+                          size: 30,
+                        )),
+                    Text(
+                      "$count",
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          setState(() {
+                            count++;
+                          });
+                        },
+                        icon: Icon(
+                          Icons.add_circle_outline,
+                          color: const Color.fromARGB(255, 66, 252, 169),
+                          size: 30,
+                        )),
+                  ],
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: count > 0
+                        ? () {
+                            // Add to cart logic
+                          }
+                        : null,
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 20, 54, 64),
+                        padding: const EdgeInsets.symmetric(horizontal: 40)),
+                    child: Text("Add to Cart".tr,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          fontStyle: FontStyle.normal,
+                          color: Color.fromARGB(255, 66, 252, 169),
+                        ))),
+              )
+            ],
+          )),
+    );
   }
 }
