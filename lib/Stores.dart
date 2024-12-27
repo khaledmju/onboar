f6c94378d2a7ec8a9d08e0d6ac2d3ecec694c528
khaledmju <khaledjoouu@gmail.com>
2024-12-27 21:33:08 +0300

    c

--- lib/Stores.dart
+++ lib/Stores.dart
@@ -5,6 +5,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Cart.dart';
+import 'FavoritesController.dart';
 import 'FavoritesPage.dart';
 import 'ProductsPage.dart';
 import 'Settings.dart';
@@ -110,6 +111,7 @@ class HomeContent extends StatefulWidget {
   @override
   State<HomeContent> createState() => _HomeContentState();
 }
+final FavoritesController favoritesController = Get.find();
 
 class _HomeContentState extends State<HomeContent> {
   int currentPage = 0 ;
@@ -147,6 +149,11 @@ class _HomeContentState extends State<HomeContent> {
     "images/image_applications.jpg",
   ];
 
+  @override
+  void initState() {
+    favoritesController.loadFavorites();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
