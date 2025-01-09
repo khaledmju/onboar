b96de2ecc4de15a059072e123566e5796ea1a2c3
khaledmju <khaledjoouu@gmail.com>
2025-01-09 18:50:20 +0300

    c

--- lib/Driver.dart
+++ lib/Driver.dart
@@ -11,12 +11,7 @@ class Driver extends StatefulWidget{
   }
 }
 class DriverState extends State<Driver>{
-//   String? driverLogo;
-//   @override
-//   void initState() {
-//     driverLogo = prefs!.getString("driverLogo ");
-//     super.initState();
-//   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,35 +39,3 @@ class DriverState extends State<Driver>{
         shadowColor: const Color.fromARGB(255, 48, 193, 152),
       ),
       body: Center(child:Text("Driver"),),);}}
-//       body: Center(
-//         child: driverLogo != null
-//             ? Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             ClipOval(
-//               child: Image.network(
-//                 driverLogo!,
-//                 height: 150,
-//                 width: 150,
-//                 fit: BoxFit.cover,
-//                 errorBuilder: (context, error, stackTrace) {
-//                   return Icon(
-//                     Icons.account_circle,
-//                     size: 150,
-//                     color: Colors.grey,
-//                   );
-//                 },
-//               ),
-//             ),
-//             const SizedBox(height: 20),
-//             const Text(
-//               "Welcome, Driver!",
-//               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-//             ),
-//           ],
-//         )
-//             : const CircularProgressIndicator(), // Show a loader while retrieving the logo
-//       ),
-//     );
-//   }
-// }
